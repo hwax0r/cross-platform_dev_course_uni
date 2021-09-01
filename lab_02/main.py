0921a264483a6647aec2916041b6c142d3ed702c
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-09-01 16:03:23 +0300

    lab_02 made 70%. need to add simple solution

---
+++ lab_02/main.py
@@ -0,0 +1,80 @@
+"""
+Created on Wed 1 Sep 11:05
+
+@author: Sergeev David Evgenievich
+Group: IVT-41-18
+"""
+
+
+import os
+import shutil
+
+
+HASH_BASE: int = 28657
+HASH_MOD: int = 100000
+
+
+def hash_(to_be_processed: str, base: int, mod: int):
+    hash_calc = 0
+    for char in to_be_processed:
+        symbol: int = ord(char)
+        hash_calc = (hash_calc * base % mod + symbol) % mod
+    return hash_calc
+
+
+def my_walker(target_directory: str):
+    """
+    Заданием предусмотрено разрешение коллизий названий файлов
+    добавлением к началу названия файла соответствующую папку,
+    в которой он находится, а также папки уровней выше вплоть
+    до указанной пользователем директории.
+    Коллизии - файлы с одинаковым названием
+
+    Однако, есть ограничение на длину названия файла, это 260 символов.
+    В таком случае предлагаю разрешать вложенность с помощью
+    хэширования названия пути и добавления хэша в начало названия файла.
+    Таким образом, в случае необходимости,пользователь сможет найти
+    оригинальный путь файла в хэш-таблице или сгенерированном файле.
+    """
+
+    roots = dict()
+
+    for current_path, folders, files in os.walk(target_directory):
+        for file in files:
+            # чистка от системных файлов
+            file_name, file_extension = os.path.splitext(file)
+            if file_name == '.DS_Store' or file_extension == '.DS_Store':
+                continue
+
+            local_path = current_path[:len(target_directory)]
+
+            # перенос файла из директории файла в исходную
+            # выполняется, если файл не в исходной директории
+            if local_path != '':
+                path_to_file: int = hash_(current_path[len(target_directory):], HASH_BASE, HASH_MOD)
+                if path_to_file == 0:
+                    continue
+                if path_to_file not in roots.keys():
+                    roots.update({path_to_file: current_path[len(target_directory):]+'/'})
+
+                new_filename = f'{path_to_file}_{file}'
+                shutil.move(f'{current_path}/{file}', f'{target_directory}/{new_filename}')
+
+    # удаление папок в целевой директории
+    target_directory_subfolders = [folder.path for folder in os.scandir(target_directory) if folder.is_dir()]
+    for folder_path in target_directory_subfolders:
+        shutil.rmtree(folder_path)
+
+    # отчёт о ключах, кодирующих путь файлов
+    roots_report = open(f'{target_directory}/work_report.txt', 'a')
+    for key, value in sorted(roots.items()):
+        print("{}: {}".format(key, value), file=roots_report)
+
+
+def main():
+    target_dir: str = os.getcwd() + '/dir_after_script'
+    my_walker(target_dir)
+
+
+if __name__ == '__main__':
+    main()
