c9d66310a0e3e7cbd65f72b7e871dd1edf735fde
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-09-01 20:38:47 +0300

    lab_02 done 100%

--- lab_02/main.py
+++ lab_02/main.py
@@ -5,13 +5,26 @@ Created on Wed 1 Sep 11:05
 Group: IVT-41-18
 """
 
-
 import os
 import shutil
+import time
 
 
 HASH_BASE: int = 28657
 HASH_MOD: int = 100000
+
+# path separator depends on OS
+SEPARATOR: chr = os.sep
+
+
+def timeit(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print(f'func: {f.__name__} took: {te-ts:2.4f} sec')
+        return result
+    return timed
 
 
 def hash_(to_be_processed: str, base: int, mod: int):
@@ -22,7 +35,8 @@ def hash_(to_be_processed: str, base: int, mod: int):
     return hash_calc
 
 
-def my_walker(target_directory: str):
+@timeit
+def my_solution(target_directory: str):
     """
     Заданием предусмотрено разрешение коллизий названий файлов
     добавлением к началу названия файла соответствующую папку,
@@ -55,10 +69,10 @@ def my_walker(target_directory: str):
                 if path_to_file == 0:
                     continue
                 if path_to_file not in roots.keys():
-                    roots.update({path_to_file: current_path[len(target_directory):]+'/'})
+                    roots.update({path_to_file: current_path[len(target_directory):] + SEPARATOR})
 
                 new_filename = f'{path_to_file}_{file}'
-                shutil.move(f'{current_path}/{file}', f'{target_directory}/{new_filename}')
+                shutil.move(f'{current_path}{SEPARATOR}{file}', f'{target_directory}{SEPARATOR}{new_filename}')
 
     # удаление папок в целевой директории
     target_directory_subfolders = [folder.path for folder in os.scandir(target_directory) if folder.is_dir()]
@@ -66,14 +80,41 @@ def my_walker(target_directory: str):
         shutil.rmtree(folder_path)
 
     # отчёт о ключах, кодирующих путь файлов
-    roots_report = open(f'{target_directory}/work_report.txt', 'a')
+    roots_report = open(f'{target_directory}{SEPARATOR}work_report.txt', 'a')
     for key, value in sorted(roots.items()):
         print("{}: {}".format(key, value), file=roots_report)
 
 
+@timeit
+def recommended_solution(target_directory: str):
+    for current_path, folders, files in os.walk(target_directory):
+        for file in files:
+            # чистка от системных файлов
+            file_name, file_extension = os.path.splitext(file)
+            if file_name == '.DS_Store' or file_extension == '.DS_Store':
+                continue
+
+            local_path = current_path[len(target_directory):]
+
+            # перенос файла из директории файла в исходную
+            # выполняется, если файл не в исходной директории
+            if local_path != '':
+                local_path = local_path.replace(SEPARATOR, '_')
+                new_filename = f'{local_path}_{file}'
+                shutil.move(f'{current_path}{SEPARATOR}{file}', f'{target_directory}{SEPARATOR}{new_filename}')
+
+    # удаление папок в целевой директории
+    target_directory_subfolders = [folder.path for folder in os.scandir(target_directory) if folder.is_dir()]
+    for folder_path in target_directory_subfolders:
+        shutil.rmtree(folder_path)
+
+
 def main():
-    target_dir: str = os.getcwd() + '/dir_after_script'
-    my_walker(target_dir)
+    target_dir: str = f'{os.getcwd()}{SEPARATOR}my_solution'
+    my_solution(target_directory=target_dir)  # result in ./my_solution
+
+    target_dir: str = f'{os.getcwd()}{SEPARATOR}recommended_solution'
+    recommended_solution(target_directory=target_dir)  # result in ./recommended_solution
 
 
 if __name__ == '__main__':
